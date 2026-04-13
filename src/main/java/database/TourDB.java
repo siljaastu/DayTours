f98d8983cf8e2200fc1d12d156f4eeeb647c0486
María Gunnarsdóttir <mariagunnarsdottir@htgi-dw1000.rhi.hi.is>
2026-04-13 10:03:48 +0000

    Ýmislegt, reyndi að run-a tourdb en það virkar ekki því þetta er ekki maven project. Reyndi að bæta við pom.xml en það virkar ennþá ekki

--- src/main/java/database/TourDB.java
+++ src/main/java/database/TourDB.java
@@ -7,14 +7,23 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class TourDB {
-    private String url = "jdbc:sqlite:tours.db";
+    private String url = "jdbc:sqlite:src/main/java/database/tours.db";
 
     // Helper method to open connection
     private Connection connect() throws SQLException {
         return DriverManager.getConnection(url);
     }
 
-    public ArrayList<Tour> select(String keyword) {
+    /**
+     * Looks up tours in DB based on a keyword and returns all
+     * tours that have that keyword in the title.
+     *
+     * @param keyword The search parameter
+     * @return All tours that have the keyword in their title
+     * @throws SQLeseption
+     */
+
+    public ArrayList<Tour> search(String keyword) {
         ArrayList<Tour> results = new ArrayList<>();
         String query = "SELECT * FROM tours WHERE name LIKE ?";
 
@@ -51,8 +60,19 @@ public class TourDB {
 
     }
 
+    public ArrayList<Tour> filterByRegion(String region) {
+        ArrayList<Tour> results = new ArrayList<>();
+        return results;
+    }
+
     public static void main(String[] args)
             throws Exception {
+
+        TourDB prufa = new TourDB();
+        ArrayList<Tour> tours = new ArrayList<>(prufa.search("northern"));
+        for (Tour tour : tours) {
+            System.out.println(tour.getTourName() + " " + tour.getTourType() + " " + tour.getPrice());
+        }
 
     }
 
